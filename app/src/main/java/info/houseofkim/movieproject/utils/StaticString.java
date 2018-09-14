8524f63766c6eb3dacf240878b3861f581431ae8
kronikus <andrew@houseofkim.com>
2018-09-13 21:09:39 -0400

    git fix

--- app/src/main/java/info/houseofkim/movieproject/utils/StaticString.java
+++
@@ -1,7 +0,0 @@
-package info.houseofkim.movieproject.utils;
-
-public abstract class StaticString {
-   private static String key = "d76a04c903c6ca819c49890ed0ccc14a";
-
-   public static String getKey(){return key;}
-}
